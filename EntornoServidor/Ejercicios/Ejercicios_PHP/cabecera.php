345d0f3b96ab4482df3c1128c6fcd6e1658c02e0
Borja <fbdelacruzl01@educarex.es>
2025-10-08 18:14:05 +0200

    añadiendo antes de recreo

---
+++ EntornoServidor/Ejercicios/Ejercicios_PHP/cabecera.php
@@ -0,0 +1,43 @@
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="/cached.php?t=1756715876&amp;f=/fonts/Fira/fira.css" media="screen">
+      <link rel="stylesheet" type="text/css" href="/cached.php?t=1756715876&amp;f=/fonts/Font-Awesome/css/fontello.css" media="screen">
+      <link rel="stylesheet" type="text/css" href="/cached.php?t=1759298403&amp;f=/styles/theme-base.css" media="screen">
+      <link rel="stylesheet" type="text/css" href="/cached.php?t=1756715876&amp;f=/styles/theme-medium.css" media="screen">
+  
+ <link rel="icon" type="image/svg+xml" sizes="any" href="https://www.php.net/favicon.svg?v=2">
+ <link rel="icon" type="image/png" sizes="196x196" href="https://www.php.net/favicon-196x196.png?v=2">
+ <link rel="icon" type="image/png" sizes="32x32" href="https://www.php.net/favicon-32x32.png?v=2">
+ <link rel="icon" type="image/png" sizes="16x16" href="https://www.php.net/favicon-16x16.png?v=2">
+ <link rel="shortcut icon" href="https://www.php.net/favicon.ico?v=2">
+
+ <link rel="search" type="application/opensearchdescription+xml" href="https://www.php.net/phpnetimprovedsearch.src" title="Add PHP.net search">
+ <link rel="alternate" type="application/atom+xml" href="https://www.php.net/releases/feed.php" title="PHP Release feed">
+ <link rel="alternate" type="application/atom+xml" href="https://www.php.net/feed.atom" title="PHP: Hypertext Preprocessor">
+
+ <link rel="canonical" href="https://www.php.net/manual/es/index.php">
+ <link rel="shorturl" href="https://www.php.net/manual/es/index.php">
+ <link rel="alternate" href="https://www.php.net/manual/es/index.php" hreflang="x-default">
+
+ <link rel="contents" href="https://www.php.net/manual/es/index.php">
+ <link rel="index" href="https://www.php.net/manual/es/">
+ <link rel="prev" href="https://www.php.net/manual/es/">
+ <link rel="next" href="https://www.php.net/manual/es/">
+
+ <link rel="alternate" href="https://www.php.net/manual/en/index.php" hreflang="en">
+ <link rel="alternate" href="https://www.php.net/manual/de/index.php" hreflang="de">
+ <link rel="alternate" href="https://www.php.net/manual/es/index.php" hreflang="es">
+ <link rel="alternate" href="https://www.php.net/manual/fr/index.php" hreflang="fr">
+ <link rel="alternate" href="https://www.php.net/manual/it/index.php" hreflang="it">
+ <link rel="alternate" href="https://www.php.net/manual/ja/index.php" hreflang="ja">
+ <link rel="alternate" href="https://www.php.net/manual/pt_BR/index.php" hreflang="pt_BR">
+ <link rel="alternate" href="https://www.php.net/manual/ru/index.php" hreflang="ru">
+ <link rel="alternate" href="https://www.php.net/manual/tr/index.php" hreflang="tr">
+ <link rel="alternate" href="https://www.php.net/manual/uk/index.php" hreflang="uk">
+ <link rel="alternate" href="https://www.php.net/manual/zh/index.php" hreflang="zh">
+
+<link rel="stylesheet" type="text/css" href="/cached.php?t=1756715876&amp;f=/fonts/Fira/fira.css" media="screen">
+<link rel="stylesheet" type="text/css" href="/cached.php?t=1756715876&amp;f=/fonts/Font-Awesome/css/fontello.css" media="screen">
+<link rel="stylesheet" type="text/css" href="/cached.php?t=1759298403&amp;f=/styles/theme-base.css" media="screen">
+<link rel="stylesheet" type="text/css" href="/cached.php?t=1756715876&amp;f=/styles/theme-medium.css" media="screen">
+
