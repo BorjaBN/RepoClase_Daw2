1c2578f1ab183f9d903eaca3c87c5a3ba945baba
Borja <fbdelacruzl01@educarex.es>
2025-12-02 18:54:11 +0100

    .

---
+++ EntornoServidor/evaluacion_1/viogen_poo/vistas/perfil.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Mi perfil</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+    <div >
+        <h5 >Mi perfil</h5>
+        <p><strong>Nombre:</strong> <?php echo $_SESSION['usuario']['nombre']; ?> </p>
+    </div>
+
+    <div >
+      <button>Log Out</button>
+    </div>
+    <form action=""></form>
+  </div>
+
+
+  
+
+</body>
+</html>
