cf89b0677256de6de922cea68a4e7b1b959f63dc
Borja <fbdelacruzl01@educarex.es>
2025-12-09 18:51:00 +0100

    .

--- EntornoServidor/servidorWeb/armagedon/index.php
+++ EntornoServidor/servidorWeb/armagedon/index.php
@@ -1,106 +1,21 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 //-------------------------------PARA VER POR QUÉ METODO VIENE----------------------------------------------------------
 
-    try{
-
-        $metodo = strtoupper($_SERVER['REQUEST_METHOD']);
-        //throw new Exception("Teste de Excepción");
-        switch($metodo){
-            case 'GET':
-                listar();
-                break;
-            case 'POST':
-                insertar();
-                break;
-            case 'DELETE':
-                eliminar();
-                break;
-            case 'PUT':
-                actualizar();
-                break;
-            default:
-                http_response_code(501);
-                echo 'Not Implemented (Err. 1)';
-                die();
-        }
-        die();
-
-    } catch(Throwable $exception){
-
-        http_response_code(500);
-        echo 'Error del servidor';
-
+    $metodo = $_SERVER['REQUEST_METHOD'];
+    switch($metodo){
+        case 'GET':
+            listar();
+            break;
+        case 'POST':
+            insertar();
+            break;
+        default:
+            http_response_code(501);
+            echo 'Not Implemented';
+            die();
     }
-
-
-//-------------------------------TODO ESTO ES EL PUT----------------------------------------------------------
-
-function actualizar(){
-    $datos = json_decode(file_get_contents('php://input'), true);
-
-    // Comprobar los parámetros
-    // Viene id
-    if (!array_key_exists('id', $_GET) || trim($_GET['id']) == ""){
-        http_response_code(400);
-        echo 'Falta el parámetro "id"';
-        die();
-    }
-    $id = $_GET['id'];
-
-    // Viene titulo
-    if (!array_key_exists('titulo', $datos) || trim($datos['titulo']) == ""){
-        http_response_code(400);
-        echo 'Falta el parámetro "titulo"';
-        die();
-    }
-
-    if (!array_key_exists('bajas', $datos) || trim($datos['bajas']) == ""){
-        http_response_code(400);
-        echo 'Falta el parámetro "bajas"';
-        die();
-    }
-
-    if (!array_key_exists('bajas', $datos) || !is_numeric($datos['bajas']) || $datos['bajas'] < 0){
-        http_response_code(400);
-        echo 'Parámetro "bajas" erróneo';
-        die();
-    }
-
-
-    //Llamamos al modelo con los datos y actualizamos el recurso
-    // nos devuelve el id del recurso insertado.
-    $respuesta = ["url" => "http://localhost/servidorWeb/armagedon/?id=$id"];
-    http_response_code(200);
-    echo json_encode($respuesta, JSON_UNESCAPED_SLASHES);
-
-}
-//-------------------------------TODO ESTO ES EL DELETE----------------------------------------------------------
-
-function eliminar(){
-    // Comprobar los parámetros
-    // Viene titulo
-    if (!array_key_exists('id', $_GET) || trim($_GET['id']) == ""){
-        http_response_code(400);
-        echo "Falta el parámetro id";
-        die();
-    }
-    echo $_GET['id'];
-    //Llamo al modelo
-    //Si no existe el elemento
-    $existe_recurso = false;
-    if(!$existe_recurso){
-        http_response_code(404);
-        echo "No existe un armagedón con ese id ($id)";
-        die();
-    }
-    //Borrado
-    http_response_code(204);
     die();
-
-}
 
 //-------------------------------TODO ESTO ES EL GET----------------------------------------------------------
 
@@ -132,7 +47,7 @@ function insertar(){
         die();
     }
 
-    if (!array_key_exists('bajas', $datos) || !is_numeric($datos['bajas']) || $datos['bajas'] < 0){
+    if (array_key_exists('bajas', $datos) || !is_numeric($datos['bajas']) || $datos['bajas'] < 0){
         http_response_code(400);
         echo 'Parámetro "bajas" erróneo';
         die();
@@ -141,7 +56,7 @@ function insertar(){
 
     //Llamamos al modelo con los datos y nos devuelve el id del recurso insertado.
     $id_insertado = 42;
-    $respuesta = ["url" => "http://localhost/servidorWeb/armagedon/?id=$id_insertado"];
+    $respuesta = ["url" => "http:\/\/localhost\/servidorWeb\/armagedon\/?id=$id_insertado"];
     http_response_code(201);
-    echo json_encode($respuesta, JSON_UNESCAPED_SLASHES);
+    echo json_encode($respuesta, true);
 }
