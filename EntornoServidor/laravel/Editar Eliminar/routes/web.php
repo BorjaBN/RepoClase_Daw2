48171e0e78f46779265e0056dc7aad2cd5039353
Borja <fbdelacruzl01@educarex.es>
2026-01-16 18:04:03 +0100

    Añadiendo cositas de laravel sin crear proyecto

---
+++ EntornoServidor/laravel/Editar Eliminar/routes/web.php
@@ -0,0 +1,8 @@
+use App\Http\Controllers\BuloController;
+
+Route::get('/', [BuloController::class, 'index']);
+Route::post('/bulos', [BuloController::class, 'store']);
+
+Route::get('/bulos/{bulo}/edit', [BuloController::class, 'edit']);
+Route::put('/bulos/{bulo}', [BuloController::class, 'update']);
+Route::delete('/bulos/{bulo}', [BuloController::class, 'destroy']);
